26307704bb97db5d12848939849f673d6a0e377c
tenshi <alehandroglushko@gmail.com>
2025-08-24 16:30:29 +0300

    introduce Swagger

--- TechnoShop/Program.cs
+++ TechnoShop/Program.cs
@@ -2,6 +2,7 @@ using Domain.Interfaces;
 using Infrastructure.Services;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
+using TechnoShop.ServicesExtensions;
 
 var builder = WebApplication.CreateBuilder(args);
 var configuration = builder.Configuration;
@@ -10,13 +11,15 @@ builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 builder.Services.AddDbContext<TechnoShopContext>(p =>
     p.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
+builder.Services.SwaggerConfigure();
 builder.Services.AddScoped<IGpuService, GpuService>();
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
---
+++ TechnoShop/ServicesExtensions/SwaggerExtension.cs
@@ -0,0 +1,45 @@
+﻿using System.Reflection;
+using Microsoft.OpenApi.Models;
+namespace TechnoShop.ServicesExtensions;
+
+public static class SwaggerExtension
+{
+    public static IServiceCollection SwaggerConfigure(this IServiceCollection serviceCollection)
+    {
+        serviceCollection.AddSwaggerGen(options =>
+        {
+            options.SwaggerDoc("v1", new OpenApiInfo()
+            {
+                Version = "v1",
+                Title = "TechnoShop API",
+                Description = "TechnoShop API"
+            });
+            options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                In = ParameterLocation.Header,
+                Description = "Please enter a valid token",
+                Name = "Authorization",
+                Type = SecuritySchemeType.Http,
+                BearerFormat = "JWT",
+                Scheme = "Bearer"
+            });
+            options.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                    },
+                    new string[]{}
+                }
+            });
+            var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+            options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+        });
+        return serviceCollection;
+    }
+}
